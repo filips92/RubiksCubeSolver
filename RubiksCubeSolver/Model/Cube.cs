a1a8968cfa18a084dd201d8c91d3ed7e4c088db9
fillips92 <filsmu@wp.pl>
2017-11-27 22:58:38 +0100

    Basic model

---
+++ RubiksCubeSolver/Model/Cube.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubiksCubeSolver.Model
+{
+    public class Cube
+    {
+        public Face LeftFace { get; set; }
+        public Face RightFace { get; set; }
+        public Face FrontFace { get; set; }
+        public Face RearFace { get; set; }
+        public Face UpperFace { get; set; }
+        public Face BottomFace { get; set; }
+    }
+}
---
+++ RubiksCubeSolver/Model/Face.cs
@@ -0,0 +1,28 @@
+﻿namespace RubiksCubeSolver.Model
+{
+    public class Face
+    {
+        public Colors[,] Tiles { get; set; }
+        public Face()
+        {
+            Tiles = new Colors[2,2];
+        }
+        public Face(Colors color)
+        {
+            Tiles = new Colors[2, 2] { 
+                { color, color }, 
+                { color, color }
+            };
+        }
+    }
+
+    public enum Colors
+    {
+        White,
+        Yellow,
+        Red,
+        Orange,
+        Blue,
+        Green
+    }
+}
